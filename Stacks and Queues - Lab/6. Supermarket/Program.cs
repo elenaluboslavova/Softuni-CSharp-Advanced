d16e065c07ff2caa8ba58a529a06080b3a3f70a1
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-09-16 21:00:11 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            int count = 0;
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var item in queue)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    queue.Clear();
+                    count = 0;
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                    count++;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} people remaining.");
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            string command = Console.ReadLine();
+
+            int counter = 0;
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (queue.Count == 0) break;
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        counter++;
+                    }
+
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
