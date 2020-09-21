ab1e82ae0bcddaebb4d5ff29307188c526f9575b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-09-21 14:36:07 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = sizes[0];
+            int cols = sizes[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] data = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+            int sum = 0;
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    sum = 0;
+                    sum = matrix[i, j] + matrix[i + 1, j] + matrix[i, j + 1] + matrix[i + 1, j + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[maxRow, maxCol]} {matrix[maxRow, maxCol + 1]}");
+            Console.WriteLine($"{matrix[maxRow + 1, maxCol]} {matrix[maxRow + 1, maxCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jaggedMatrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jaggedMatrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] input = command.Split();
+                string action = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+                int value = int.Parse(input[3]);
+
+                if ((row >= n || row < 0) || (col > jaggedMatrix[row].Length - 1 || col < 0))
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    switch (action)
+                    {
+                        case "Add":
+                            jaggedMatrix[row][col] += value;
+                            break;
+
+                        case "Subtract":
+                            jaggedMatrix[row][col] -= value;
+                            break;
+                    }
+                }
+                
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(jaggedMatrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
