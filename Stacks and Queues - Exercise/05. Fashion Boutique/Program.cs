477bbe75c637a5f0a791f77a7f134341280ada73
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-09-22 13:09:03 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(clothes);
+            int capacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int racks = 1;
+            while (stack.Count > 0)
+            {
+                int current = stack.Peek();
+                sum += current;
+                if (sum <= capacity)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    racks++;
+                    sum = 0;
+                }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ").ToArray();
+            Queue<string> queue = new Queue<string>(songs);
+
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+                if (command.Contains("Play"))
+                {
+                    queue.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string[] comand = command.Split("Add ").ToArray();
+                    string song = comand[1];
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", queue));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
