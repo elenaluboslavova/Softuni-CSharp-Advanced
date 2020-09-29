f3876397ea0d317243a637bff6cdfab5a680939e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-09-29 20:49:22 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> set = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string direction = command.Split(", ")[0];
+                string number = command.Split(", ")[1];
+
+                if (direction == "IN")
+                {
+                    set.Add(number);
+                }
+                else
+                {
+                    if (set.Contains(number))
+                    {
+                        set.Remove(number);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            if (set.Count > 0)
+            {
+                foreach (var item in set)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> reservations = new HashSet<string>();
+
+            while (true)
+            {
+                if (input == "PARTY")
+                {
+                    while (input != "END")
+                    {
+                        reservations.Remove(input);
+                        input = Console.ReadLine();
+                    }
+                    Console.WriteLine(reservations.Count);
+                    foreach (var item in reservations)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Environment.Exit(0);
+                }
+                else if (input.Length == 8 || char.IsDigit(input[0]))
+                {
+                    reservations.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
