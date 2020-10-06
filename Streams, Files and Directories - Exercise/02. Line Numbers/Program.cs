2b7f7f192701bf090205973c0dc1a624a8e6c78b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-06 10:50:56 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Exercise/02. Line Numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../../text.txt");
+
+            string[] result = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int countOfLetters = CountOfLetters(line);
+                int countOfMarks = CountOfPunctuationMarks(line);
+
+                result[i] = $"Line {i + 1}: {line} ({countOfLetters})({countOfMarks})";
+            }
+            File.WriteAllLines("../../../Output.txt", result);
+        }
+
+        static int CountOfLetters(string line)
+        {
+            int counter = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currentSymbol = line[i];
+                if (char.IsLetter(currentSymbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        static int CountOfPunctuationMarks(string line)
+        {
+            char[] punctuationMarks = { '-', ',', '.', '!', '?', '\'', ':', ';' };
+            int counter = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currentSymbol = line[i];
+                if (punctuationMarks.Contains(currentSymbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercise/03. Word Count/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = File.ReadAllLines("../../../words.txt");
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                if (!list.Contains(word))
+                {
+                    list.Add(word);
+                }
+            }
+
+            string[] text = File.ReadAllLines("../../../text.txt");
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string[] current = text[i].Split(new char[] { ' ', '.', ',', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < current.Length; j++)
+                {
+                    if (list.Contains(current[j].ToLower()))
+                    {
+                        if (!dict.ContainsKey(current[j].ToLower()))
+                        {
+                            dict.Add(current[j].ToLower(), 1);
+                        }
+                        else
+                        {
+                            dict[current[j].ToLower()]++;
+                        }
+                    }
+                }
+            }
+
+            List<string> currents = new List<string>();
+            foreach (var item in dict.OrderByDescending(x => x.Value))
+            {
+                string current = $"{item.Key} - {item.Value}";
+                currents.Add(current);
+                File.WriteAllLines("../../../actualResult.txt", currents);
+            }
+        }
+    }
+}
