07ea9dda532fe36d8c08587a6518c68cb2c1bb1e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-06 10:53:42 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Exercise/06. Zip and Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"D:\ZipDemo", @"D:\ZipDemo2\myZipFile.zip");
+            ZipFile.ExtractToDirectory(@"D:\ZipDemo2\myZipFile.zip", @"D:\ZipDemo3");
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercise/Streams, Files and Directories - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Streams__Files_and_Directories___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
