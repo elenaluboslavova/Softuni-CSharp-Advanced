4bf23febd764a61c434ec45d55b3360b6700b35e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-09 17:00:43 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<int> list = new List<int>();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                bool isT = true;
+
+                foreach (var item in numbers)
+                {
+                    Predicate<int> isDivisible = x => i % x == 0;
+
+                    if (!isDivisible(item))
+                    {
+                        isT = false;
+                        break;
+                    }
+                }
+                if (isT)
+                {
+                    list.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> people = Console.ReadLine().Split().ToList();
+
+            Func<string, int> getAsciiSum = p => p.Select(c => (int)c).Sum();
+
+            Func<List<string>, int, Func<string, int>, string> nameFunc = (people, n, func) => people.FirstOrDefault(p => func(p) >= n);
+
+            string result = nameFunc(people, n, getAsciiSum);
+            Console.WriteLine(result);
+        }
+
+        static string GetName(List<string> people, int n, Func<string, int> func)
+        {
+            string result = null;
+            foreach (string person in people)
+            {
+                if (func(person) >= n)
+                {
+                    result = person;
+                }
+            }
+            return result;
+        }
+    }
+}
