592f9001d58e658881000a886e19c30d022df69c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-19 13:39:12 +0300

    Add files via upload

---
+++ Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var scale = new EqualityScale<int>(5, 6);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
