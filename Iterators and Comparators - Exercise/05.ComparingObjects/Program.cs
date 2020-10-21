949b944d3470ee4345c521e0052f2dfec399bc1a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-21 17:00:22 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> people = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] splitted = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = splitted[0];
+                int age = int.Parse(splitted[1]);
+                string town = splitted[2];
+
+                Person person = new Person(name, age, town);
+                people.Add(person);
+                input = Console.ReadLine();
+            }
+            int n = int.Parse(Console.ReadLine());
+            Person comparedPerson = people[n - 1];
+
+            int samePersonCount = 0;
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(comparedPerson) == 0)
+                {
+                    samePersonCount++;
+                }
+            }
+            if (samePersonCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                int notSamePersonCount = people.Count - samePersonCount;
+                Console.WriteLine($"{samePersonCount} {notSamePersonCount} {people.Count}");
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfSortedPeople = new SortedSet<Person>();
+            var listOfHashedPeople = new HashSet<Person>();
+            var countPeople = int.Parse(Console.ReadLine());
+            for (var i = 0; i < countPeople; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var name = input[0];
+                var age = int.Parse(input[1]);
+                var person = new Person(name, age);
+                listOfHashedPeople.Add(person);
+                listOfSortedPeople.Add(person);
+            }
+
+            Console.WriteLine(listOfHashedPeople.Count);
+            Console.WriteLine(listOfSortedPeople.Count);
+        }
+    }
+}
